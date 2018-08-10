aa44ef6c622f0220af64b716b954438795bca77c
William Pitcock <nenolod@dereferenced.org>
2018-08-09 22:30:47 -0500

    check: do full scan, more useful

--- check.py
+++ check.py
@@ -3,26 +3,23 @@ import asyncio
 
 
 def usage():
-    print('usage: python3 check.py config ip username password')
+    print('usage: python3 check.py config ip')
     exit()
 
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 3:
     usage()
 
 
-from antissh import check_with_credentials, TARGET_IP, TARGET_PORT
+from antissh import fetch_banner, check_with_credentials_shallow, TARGET_IP, TARGET_PORT
 
 
 def main():
     ip = sys.argv[2]
-    username = sys.argv[3]
-    password = sys.argv[4]
 
     print('checking', ip)
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(check_with_credentials(ip, TARGET_IP, TARGET_PORT, username, password))
-
+    result = loop.run_until_complete(check_with_credentials_shallow(ip, TARGET_IP, TARGET_PORT))
     print('result:', result)
 
 
